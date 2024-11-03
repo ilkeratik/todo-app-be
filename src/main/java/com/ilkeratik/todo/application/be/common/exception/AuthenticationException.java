eb799f69b1c1a2acc8a3186b612d4b83fbb3a6c4
ilkeratik <ilkeratik35@gmail.com>
2024-11-03 11:08:40 +0000

    TODOAPP-4 using spring-boot-starter-oauth2-resource-server for validating tokens

---
+++ src/main/java/com/ilkeratik/todo/application/be/common/exception/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.ilkeratik.todo.application.be.common.exception;
+
+public class AuthenticationException extends RuntimeException {
+
+  public AuthenticationException(String s) {
+  }
+}
--- src/main/java/com/ilkeratik/todo/application/be/config/security/WebSecurityConfig.java
+++ src/main/java/com/ilkeratik/todo/application/be/config/security/WebSecurityConfig.java
@@ -1,38 +1,33 @@
 package com.ilkeratik.todo.application.be.config.security;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
-  private final JwtAuthenticationFilter jwtAuthenticationFilter;
-
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+    http
         // Disable CSRF protection as it's not needed for stateless APIs
         .csrf(AbstractHttpConfigurer::disable)
         // Disable session creation
         .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth ->
-            auth.requestMatchers("/actuator/**", "/v3/api-docs/**", "/swagger-ui/**",
-                    "/swagger-ui.html")
+            auth.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
                 .permitAll()
-                .anyRequest().authenticated()
-        )
-        .httpBasic(withDefaults());
+                .anyRequest()
+                .authenticated()
+        ).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
     return http.build();
   }
