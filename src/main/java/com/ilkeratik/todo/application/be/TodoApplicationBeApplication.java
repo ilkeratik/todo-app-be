42be2ce7de7d4905117c7491cdbbf51eb469aaf2
ilkeratik <ilkeratik35@gmail.com>
2024-11-03 21:41:55 +0000

    TODOAPP-4 add encrypted application properties

--- src/main/java/com/ilkeratik/todo/application/be/TodoApplicationBeApplication.java
+++ src/main/java/com/ilkeratik/todo/application/be/TodoApplicationBeApplication.java
@@ -1,11 +1,14 @@
 package com.ilkeratik.todo.application.be;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@OpenAPIDefinition(info = @Info(title = "ToDo Application API", version = "1.0", description = "API Docs for ToDo Application"))
+
+@OpenAPIDefinition(info = @Info(title = "To do Application API", version = "1.0", description = "API Docs for To do application"))
 @SpringBootApplication
+@EnableEncryptableProperties
 public class TodoApplicationBeApplication {
 
   public static void main(String[] args) {
